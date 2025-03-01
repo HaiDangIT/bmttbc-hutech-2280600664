f225d70ba29a7451acbf29178535395db04a8894
Hai Dang <ledang3916@gmail.com>
2025-03-01 10:24:22 +0700

    code 6

--- Bai2/ex03_06.py
+++ Bai2/ex03_06.py
@@ -1,21 +1,28 @@
-# def xoaphantu (dictionary,key):
-#     if key in dictionary:
-#         del dictionary[key]
-#         return True
-#     else:
-#         return False
-    
-   
-def nhapphantu (dict):
-    dict.key = input("Enter key: ")
-    dict.value = input("Enter value: ")
-def xoaphantu (dict2):
-    if dict2.key in dict2.dictionary:
-        del dict2.dictionary[dict2.key]
+# class Mydict:
+#     def nhapphantu (dict):
+#         dict.key = input("Enter key: ")
+#         dict.value = input("Enter value: ")
+def xoaphantu (dictionary,key):
+    if key in dictionary:
+        del dictionary[key]
         return True
     else:
         return False
-  
+
 
     #su dung ham va in ket qua
+    
+input_list = int(input("Nhap so key ban muon tao: "))
+
+list_dict = {input("Enter key: "): input("Enter value: ") for _ in range(input_list)}
+
+input_del = input("Nhap key can xoa: ")
+
+result = xoaphantu(list_dict,input_del)
+if result:
+    print ("Phan tu da duoc xoa tu Dictionary: ", list_dict)
+else:
+    print ("Khong tim thay phan tu can xoa!!!")
+
+
     
