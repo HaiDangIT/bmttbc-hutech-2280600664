3180e598a0829336861c71888e5dd5ef917d46ba
HaiDangIT <ledang3916@gmail.com>
2025-03-15 07:53:49 +0700

    [Add] Bai 03

--- Bai1/ex02_02.py
+++
@@ -1,6 +0,0 @@
-#Nhap ban kinh
-bankinh = float(input("Nhap ban kinh cua hinh tron: "))
-#tinh dien tich
-dientich = 3.14 * (bankinh ** 2)
-#in dien tich
-print ("Dien tich cua hinh tron: ", dientich)
--- Bai1/ex02_10.py
+++
@@ -1,5 +0,0 @@
-def daonguocchuoi(chuoi):
-    return chuoi[::-1]
-#su dung ham va in ket qua
-inputstring = input("Moi nhap chuoi can dao nguoc: ")
-print("Chuoi dao nguoc la: ", daonguocchuoi(inputstring))
